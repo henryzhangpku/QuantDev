a6fc46aad1ec49d6bdfba9b23f52d0d0a1485d02
Henry Zhang <heng.henry.zhang@gmail.com>
2023-09-30 08:01:04 -0700

    refactor rebalancer

--- coinbase/rebalance.py
+++ coinbase/rebalance.py
@@ -4,21 +4,20 @@ from global_config import *
 if __name__ == '__main__':
     signalname = 'trend'
     pm = pd.read_csv(outpath + '/coinbase/' + signalname + '_signals.csv')
-
-
+    pm = pm[pm[pm.columns[1]]>0] # enter long only signals , or exit
+    USD = 10
     signals = set(pm['Asset'].apply(lambda x: x.split('-')[0]))
 
     print(signals)
     positions = show_holding()
 
     assets_to_exit = set(positions.index.values).difference(
-        signals)
+        signals).difference(set(['USD']))
     print("Exiting Trade:")
     print(assets_to_exit)
     for instrument_code in assets_to_exit:
         order_amount = -1
         close_ratio = 1
-        USD = 25
         tradeCB(instrument_code, order_amount, close_ratio, USD)
 
     assets_to_enter = signals.difference(
@@ -28,17 +27,8 @@ if __name__ == '__main__':
     for instrument_code in assets_to_enter:
         order_amount = 1
         close_ratio = 1
-        USD = 25
+        
         tradeCB(instrument_code, order_amount, close_ratio, USD)
 
-    assets_to_check = set(positions.index.values).intersection(
-        signals)  # .remove('USD')
-    for instrument_code in assets_to_check:
-        order_amount = 1
-        close_ratio = 0.5
-        USD = 2
-        if float(positions.loc[instrument_code]['MarketValue']) < float(USD) - 1:
-            print(positions.loc[instrument_code])
-            tradeCB(instrument_code, order_amount, close_ratio, USD)
 
 
